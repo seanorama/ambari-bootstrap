2ea460a4fc16fdf1c99ded67aa78e30a24913906
Sean Roberts <seanorama@gmail.com>
2014-12-31 22:50:50 +0000

    just sorting the service list

--- providers/aws/hdp-install.sh
+++ providers/aws/hdp-install.sh
@@ -1,12 +1,9 @@
-
 set -e
 
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 sudo yum install jq
-#wget http://stedolan.github.io/jq/download/linux64/jq
-#chmod a+x jq
 
 aws configure
 
@@ -35,32 +32,36 @@ cat > ambari.blueprint << 'EOF'
   "host_groups" : [
     { "name" : "master",
       "components" : [
-        { "name" : "NAMENODE" },
-        { "name" : "SECONDARY_NAMENODE" },
-        { "name" : "RESOURCEMANAGER" },
-        { "name" : "HISTORYSERVER" },
-        { "name" : "ZOOKEEPER_SERVER" },
-        { "name" : "GANGLIA_SERVER" },
+        { "name" : "APP_TIMELINE_SERVER" },
         { "name" : "GANGLIA_MONITOR" },
+        { "name" : "GANGLIA_SERVER" },
+        { "name" : "HCAT" },
+        { "name" : "HISTORYSERVER" },
         { "name" : "HIVE_METASTORE" },
         { "name" : "HIVE_SERVER" },
-        { "name" : "WEBHCAT_SERVER" },
-        { "name" : "HCAT" },
+        { "name" : "JOURNALNODE" },
         { "name" : "MYSQL_SERVER" },
-        { "name" : "APP_TIMELINE_SERVER" }
+        { "name" : "NAMENODE" },
+        { "name" : "NODEMANAGER" },
+        { "name" : "RESOURCEMANAGER" },
+        { "name" : "SECONDARY_NAMENODE" },
+        { "name" : "WEBHCAT_SERVER" },
+        { "name" : "ZOOKEEPER_SERVER" }
       ],
       "cardinality" : "1"
     },
     { "name" : "slaves",
       "components" : [
         { "name" : "DATANODE" },
-        { "name" : "HDFS_CLIENT" },
-        { "name" : "YARN_CLIENT" },
-        { "name" : "MAPREDUCE2_CLIENT" },
-        { "name" : "ZOOKEEPER_CLIENT" },
         { "name" : "GANGLIA_MONITOR" },
+        { "name" : "HDFS_CLIENT" },
+        { "name" : "HIVE_CLIENT" },
+        { "name" : "JOURNALNODE" },
+        { "name" : "MAPREDUCE2_CLIENT" },
+        { "name" : "NODEMANAGER" },
         { "name" : "TEZ_CLIENT" },
-        { "name" : "HIVE_CLIENT" }
+        { "name" : "YARN_CLIENT" },
+        { "name" : "ZOOKEEPER_CLIENT" }
       ],
       "cardinality" : "1+"
     } 
