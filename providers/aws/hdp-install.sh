98177fee55bfcc7cb5fe6ecec3a7042ca4f41c30
Sean Roberts <seanorama@gmail.com>
2014-12-31 22:13:01 +0000

    making it cleaner
    - using curl instead of wget
    - using yum packages
    - using official awscli instead of pip

--- providers/aws/hdp-install.sh
+++ providers/aws/hdp-install.sh
@@ -1,19 +1,25 @@
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-sudo pip install awscli
-wget http://stedolan.github.io/jq/download/linux64/jq
-chmod a+x jq
+
+set -e
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+sudo yum install jq
+#wget http://stedolan.github.io/jq/download/linux64/jq
+#chmod a+x jq
 
 aws configure
 
 # replace with the name of your deployed stack
-stackName=hdp2
+stackName=hdp-simple
 
 ambariNode=$(aws ec2 describe-instances --filters "Name=tag:aws:cloudformation:logical-id,Values=AmbariNode" "Name=tag:aws:cloudformation:stack-name,Values=$stackName" --query 'Reservations[].Instances[].[PublicIpAddress]' --output text)
 echo Ambari is available at: http://$ambariNode:8080/
 
 masterNodes=$(aws ec2 describe-instances --filters "Name=tag:aws:cloudformation:logical-id,Values=MasterNode" "Name=tag:aws:cloudformation:stack-name,Values=$stackName" --query 'Reservations[].Instances[].[PrivateDnsName]' --output text)
 workerNodes=$(aws ec2 describe-instances --filters "Name=tag:aws:cloudformation:logical-id,Values=WorkerNodes" "Name=tag:aws:cloudformation:stack-name,Values=$stackName" --query 'Reservations[].Instances[].[PrivateDnsName]' --output text)
+
+nodes=""; for node in $workerNodes $masterNodes; do nodes=$(printf '%s"%s"' "$nodes", "$node"); done; nodes="[ ${nodes#,} ]"
 
 echo creating blueprint at ./ambari.blueprint
 cat > ambari.blueprint << 'EOF'
@@ -26,7 +32,7 @@ cat > ambari.blueprint << 'EOF'
         "nagios_contact" : "admin@localhost"
       }
   },
-  "host_groups" : [ 
+  "host_groups" : [
     { "name" : "master",
       "components" : [
         { "name" : "NAMENODE" },
@@ -36,11 +42,12 @@ cat > ambari.blueprint << 'EOF'
         { "name" : "ZOOKEEPER_SERVER" },
         { "name" : "GANGLIA_SERVER" },
         { "name" : "GANGLIA_MONITOR" },
-        { "name" : "APP_TIMELINE_SERVER" },
         { "name" : "HIVE_METASTORE" },
         { "name" : "HIVE_SERVER" },
         { "name" : "WEBHCAT_SERVER" },
-        { "name" : "MYSQL_SERVER" }
+        { "name" : "HCAT" },
+        { "name" : "MYSQL_SERVER" },
+        { "name" : "APP_TIMELINE_SERVER" }
       ],
       "cardinality" : "1"
     },
@@ -48,7 +55,6 @@ cat > ambari.blueprint << 'EOF'
       "components" : [
         { "name" : "DATANODE" },
         { "name" : "HDFS_CLIENT" },
-        { "name" : "NODEMANAGER" },
         { "name" : "YARN_CLIENT" },
         { "name" : "MAPREDUCE2_CLIENT" },
         { "name" : "ZOOKEEPER_CLIENT" },
@@ -103,11 +109,11 @@ EOF
 AMBARI_CURL="curl -su admin:admin -H X-Requested-By:ambari"
 AMBARI_API="http://localhost:8080/api/v1"
 
+
 createBlueprint=$($AMBARI_CURL $AMBARI_API/blueprints/single-master -d @ambari.blueprint)
 
-createCluster=$($AMBARI_CURL $AMBARI_API/clusters/SimpleCluster -d @cluster.blueprint)
+createCluster=$($AMBARI_CURL $AMBARI_API/clusters/simple -d @cluster.blueprint)
 
-requestURL=$(echo $createCluster | ./jq '.href' | tr -d \")
+requestURL=$(echo $createCluster | jq '.href' | tr -d \")
 
-requestStatus=$($AMBARI_CURL $requestURL)
-echo $requestStatus | ./jq '.Requests.request_status'
+watch -n 10 "$AMBARI_CURL $requestURL | jq '.Requests'"
