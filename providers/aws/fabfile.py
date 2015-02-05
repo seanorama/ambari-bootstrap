98247772081a60305d194d015b1a9fa2bc66ecde
Sean Roberts <seanorama@gmail.com>
2015-02-05 00:22:31 +0000

    more fab commands

--- providers/aws/fabfile.py
+++ providers/aws/fabfile.py
@@ -3,13 +3,30 @@
 from fabric.api import local
 
 def generate_templates():
-    local('python ./cfn-generate-template.py')
+    local('with-aws.sh python ./cfn-generate-template.py')
 
 def upload_s3():
     """Stage to S3"""
     #local('aws s3 mb s3://ambari-bootstrap')
-    local("aws s3 sync --delete . s3://ambari-bootstrap/providers/aws/ \
+    local("with-aws.sh aws s3 sync --delete . s3://ambari-bootstrap/providers/aws/ \
             --exclude '.DS_Store' \
             --exclude '.git*' --exclude '*' \
             --include 'generated/*.json' \
             --include 'ambari-deploy-blueprint.sh'")
+
+def stack_create():
+    local("with-aws.sh aws cloudformation create-stack --stack-name hdp-test  \
+        --template-body file://./generated/cfn-ambari.template.json \
+        --capabilities CAPABILITY_IAM \
+        --parameters  ParameterKey=AmbariInstanceType,ParameterValue=\"m3.large\" \
+            ParameterKey=KeyName,ParameterValue=\"mb\" \
+            ParameterKey=MasterInstanceCount,ParameterValue=1 \
+            ParameterKey=WorkerInstanceCount,ParameterValue=1 \
+            ParameterKey=MasterInstanceType,ParameterValue=\"m3.large\" \
+            ParameterKey=WorkerInstanceType,ParameterValue=\"m3.large\"")
+
+def stack_describe():
+    local("with-aws.sh aws cloudformation describe-stack-resources --stack-name hdp-test")
+
+def stack_delete():
+    local("with-aws.sh aws cloudformation delete-stack --stack-name hdp-test")
