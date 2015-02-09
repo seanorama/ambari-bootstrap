974fbd0d060ce5b28dbac148fedd5f953ba887b9
Sean Roberts <seanorama@gmail.com>
2015-02-09 15:27:44 +0000

    detecting if ambari fails to start

--- ambari-bootstrap.sh
+++ ambari-bootstrap.sh
@@ -167,6 +167,7 @@ case "${lsb_dist}" in
             sed -i.orig -r 's/^[[:space:]]*hostname=.*/hostname='"${ambari_server}"'/' \
                 /etc/ambari-agent/conf/ambari-agent.ini
             ambari-agent start
+            chkconfig ambari-agent on
         fi
         if [ "${install_ambari_server}" = true ]; then
             printf "## install ambari-server\n"
@@ -176,7 +177,10 @@ case "${lsb_dist}" in
             else
                 ambari-server setup -j "${JAVA_HOME}" -s
             fi
-            ambari-server start
+            if ! nohup sh -c "service ambari-server start 2>&1 > /dev/null"; then
+                printf 'Ambari Server failed to start\n' >&2
+            fi
+            chkconfig ambari-server on
         fi
         printf "## Success! All done.\n"
         exit 0
