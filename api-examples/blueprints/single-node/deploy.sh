df259153603d025e94c6dd740b0415bbf05f33de
Sean Roberts <seanorama@gmail.com>
2015-04-07 21:19:27 +0100

    fixed cluster name to single

--- api-examples/blueprints/single-node/deploy.sh
+++ api-examples/blueprints/single-node/deploy.sh
@@ -6,7 +6,7 @@ pass=admin
 curl -u admin:${pass} \
   -H X-Requested-By:script \
   -X POST -d @blueprint.json \
-  http://${server}:8080/api/v1/blueprints/single-node
+  http://${server}:8080/api/v1/blueprints/single
 
 curl -u admin:${pass} \
   -H X-Requested-By:script \
