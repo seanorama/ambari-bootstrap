0a56b07b33de0f8b81a8cbaa8b5653e3b7f79d08
Sean Roberts <seanorama@gmail.com>
2015-07-25 22:47:48 +0100

    script to install default ambari views

---
+++ extras/ambari-views/create-views.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+## install Amabari Views with the JSON files in this directory
+##  this only works on non-kerberized clusters where all servers are on the same node
+
+source ../ambari_functions.sh
+
+ambari-configs
+
+## granting root super user rights
+${ambari_config_set} core-site hadoop.proxyuser.root.groups "users"
+${ambari_config_set} core-site hadoop.proxyuser.root.hosts "$(hostname -f)"
+
+webhdfs=$(${config_get} hdfs-site | awk -F'"' '$2 == "dfs.namenode.http-address" {print $4}' | head -1)
+
+## install views
+views="hive files pig"
+for view in ${views}; do
+  sed -e "s,webhdfs://.*:50070,webhdfs://${webhdfs}," ${view}.json > /tmp/ambari-view-${view}.json
+  ${ambari_curl}/views/${view^^}/versions/1.0.0/instances/${view~} \
+    -v -X POST -d @/tmp/ambari-view-${view}.json
+done
---
+++ extras/ambari_functions.sh
@@ -0,0 +1,33 @@
+
+## really bad hacked scripts that I've written dozens of times and
+##   finally going to start consolidating ...
+
+## todo: include my code that's in bdutil:
+##   https://github.com/GoogleCloudPlatform/bdutil/blob/master/platforms/hdp/ambari_functions.sh
+
+
+ambari_user=${ambari_user:-admin}
+ambari_pass=${ambari_password:-admin}
+ambari_protocol=${ambari_protocol:-http}
+ambari_host=${ambari_host:-localhost}
+ambari_port=${ambari_port:-8080}
+ambari_api="${ambari_protocol}://${ambari_host}:${ambari_port}/api/v1"
+ambari_curl="curl -ksSu ${ambari_user}:${ambari_pass} -H x-requested-by:sean ${ambari_api}"
+
+## auto-detect cluster
+
+ambari-get-cluster() {
+  ambari_cluster=$(${ambari_curl}/clusters \
+      | python -c 'import sys,json; \
+            print json.load(sys.stdin)["items"][0]["Clusters"]["cluster_name"]')
+}
+
+ambari-configs() {
+  ambari-get-cluster
+  ambari_configs_sh="/var/lib/ambari-server/resources/scripts/configs.sh \
+    -u ${ambari_user} -p ${ambari_pass}"
+  ambari_config_set="${ambari_configs_sh} set ${ambari_host} ${ambari_cluster}"
+  ambari_config_get="${ambari_configs_sh} get ${ambari_host} ${ambari_cluster}"
+
+  #defaultfs=$(${config_get} core-site | awk -F'"' '$2 == "fs.defaultFS" {print $4}' | head -1)
+}
