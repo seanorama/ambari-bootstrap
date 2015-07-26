98a280bdac0d08107a8da133792b5b62520081ef
Sean Roberts <seanorama@gmail.com>
2015-07-26 14:45:14 +0100

    do it

--- extras/ranger/ranger-plugin-hdfs.sh
+++ extras/ranger/ranger-plugin-hdfs.sh
@@ -14,11 +14,11 @@ webhdfs=$(${ambari_config_get} hdfs-site | awk -F'"' '$2 == "dfs.namenode.http-a
 defaultfs=$(${ambari_config_get} core-site | awk -F'"' '$2 == "fs.defaultFS" {print $4}' | head -1)
 
 ## Ranger HDFS Plugin
-echo ${ambari_config_set} ranger-hdfs-audit xasecure.audit.destination.db true
-echo ${ambari_config_set} ranger-hdfs-audit xasecure.audit.destination.hdfs.dir "${defaultfs}/ranger/audit"
-echo ${ambari_config_set} ranger-hdfs-audit xasecure.audit.is.enabled true
-echo ${ambari_config_set} ranger-hdfs-audit xasecure.audit.provider.summary.enabled true
-echo ${ambari_config_set} ranger-hdfs-plugin-properties common.name.for.certificate " "
-echo ${ambari_config_set} core-site hadoop.security.authorization true
-echo ${ambari_config_set} hdfs-site dfs.namenode.inode.attributes.provider.class org.apache.ranger.authorization.hadoop.RangerHdfsAuthorizer
-echo ${ambari_config_set} ranger-hdfs-plugin-properties ranger-hdfs-plugin-enabled yes
+${ambari_config_set} ranger-hdfs-audit xasecure.audit.destination.db true
+${ambari_config_set} ranger-hdfs-audit xasecure.audit.destination.hdfs.dir "${defaultfs}/ranger/audit"
+${ambari_config_set} ranger-hdfs-audit xasecure.audit.is.enabled true
+${ambari_config_set} ranger-hdfs-audit xasecure.audit.provider.summary.enabled true
+${ambari_config_set} ranger-hdfs-plugin-properties common.name.for.certificate " "
+${ambari_config_set} core-site hadoop.security.authorization true
+${ambari_config_set} hdfs-site dfs.namenode.inode.attributes.provider.class org.apache.ranger.authorization.hadoop.RangerHdfsAuthorizer
+${ambari_config_set} ranger-hdfs-plugin-properties ranger-hdfs-plugin-enabled yes
