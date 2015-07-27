12f43362a1ee560faa23c051f43abfbd73e1a48a
Sean Roberts <seanorama@gmail.com>
2015-07-27 12:52:03 +0100

    no message

--- extras/ranger/ranger-kms.sh
+++ extras/ranger/ranger-kms.sh
@@ -16,14 +16,14 @@ defaultfs=$(${ambari_config_get} core-site | awk -F'"' '$2 == "fs.defaultFS" {pr
 ## TODO: Check when this will be fixed.
 sudo ln -s /usr/hdp/current/hadoop-client/conf/core-site.xml /usr/hdp/current/ranger-kms/conf/
 
-sudo keytool -import -trustcacerts -alias root \
-  -noprompt -storepass changeit \
-  -file /etc/pki/ca-trust/source/anchors/activedirectory.pem \
-  -keystore /usr/hdp/current/ranger-kms/conf/ranger-plugin-keystore.jks
+#sudo keytool -import -trustcacerts -alias root \
+#  -noprompt -storepass changeit \
+#  -file /etc/pki/ca-trust/source/anchors/activedirectory.pem \
+#  -keystore /usr/hdp/current/ranger-kms/conf/ranger-plugin-keystore.jks
 
 #sudo sudo -u hdfs kinit -kt /etc/security/keytabs/hdfs.headless.keytab hdfs-$(hostname -s)
 sudo sudo -u hdfs hadoop fs -mkdir /ranger/audit/kms
-sudo sudo -u hdfs hadoop fs -chown 
+sudo sudo -u hdfs hadoop fs -chown kms /ranger/audit/kms
 
 ## Ranger KMS
 #${ambari_config_set} kms-properties REPOSITORY_CONFIG_PASSWORD "BadPass#1"
