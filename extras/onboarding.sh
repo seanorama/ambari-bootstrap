03919fe14155b26c199ddb5ea059784ba4655bfc
Sean Roberts <seanorama@gmail.com>
2015-08-30 20:45:20 +0100

    update so external users variable can be passed

--- extras/onboarding.sh
+++ extras/onboarding.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 UID_MIN=$(awk '$1=="UID_MIN" {print $2}' /etc/login.defs)
-users="$(getent passwd|awk -v UID_MIN="${UID_MIN}" -F: '$3>=UID_MIN{print $1}')"
-#users=${users:-jimmy}
+users="${users:-$(getent passwd|awk -v UID_MIN="${UID_MIN}" -F: '$3>=UID_MIN{print $1}')}"
+#export users=${users:-jimmy}
+#export users=$(ldapsearch -Q "(memberOf=CN=hadoop-users,OU=users,OU=hdp,DC=hortonworks,DC=com)" sAMAccountName | awk '/^sAMAccountName: / {print $2}')
 
 dfs_cmd="sudo sudo -u hdfs hadoop fs"
 # sudo sudo -u hdfs kinit -kt /etc/security/keytabs/hdfs.headless.keytab hdfs
