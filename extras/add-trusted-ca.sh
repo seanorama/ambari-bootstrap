90a5cab8cb782cc13e0069c233f3b394571aca3b
Sean Roberts <seanorama@gmail.com>
2015-08-31 13:34:31 +0100

    including sample ca certificate

--- extras/add-trusted-ca.sh
+++ extras/add-trusted-ca.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 ## This script imports a self-signed CA certificate
 ## to the system & java keystores
+##
+## uses my ./ca.pem by default. Replace or update $mycert with your cert
 
-## Ensure your cert exists within /etc/pki/ca-trust/source/anchors
-##   I prefer to load with automation or curl, for example:
-##   sudo curl -sSL -o ${mycert} https://....../activedirectory.pem
-mycert=${mycert:-/etc/pki/ca-trust/source/anchors/activedirectory.pem}
+## magic dirs: don't touch
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+
+## path to the certificate
+mycert=${mycert:-${__dir}/ca.pem}
 
 if [ ! -f ${mycert} ]; then
   printf "Certificate not found at: ${mycert}\nExiting.\n"
   exit 1
 fi
 
+#
+sudo cp ${mycert} /etc/pki/ca-trust/source/anchors/
 sudo yum -y install openldap-clients ca-certificates
 
-## System keystore
+## updates system & java keystores
 sudo update-ca-trust force-enable
 sudo update-ca-trust extract
 sudo update-ca-trust check
+
