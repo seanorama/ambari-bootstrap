785072a72ef01a5c6c5148cf6072895ea039c32b
Sean Roberts <seanorama@gmail.com>
2015-08-31 13:46:51 +0100

    checks if user exists

--- extras/ambari-non-root.sh
+++ extras/ambari-non-root.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-printf "y\ny\nambari\nn\nn\n" | sudo ambari-server setup
+ambari_user=${ambari_user:-ambari}
+
+getent passwd ${ambari_user} > /dev/null
+if [ $? -eq 0 ]; then
+    sudo usermod -a -G users ${ambari_user}
+    printf "y\ny\n${ambari_user}\nn\nn\n" | sudo ambari-server setup
+else
+    echo "No, the user does not exist"
+fi
+
