472961e6a8c215d7b993674e4b9d712238ec8e7d
Sean Roberts <seanorama@gmail.com>
2015-09-02 23:02:14 +0100

    cleanup

--- extras/deploy/deploy-hdp.sh
+++ extras/deploy/deploy-hdp.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 
