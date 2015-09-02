8e2511c188c9f40795d1e637c1d030f9f451dc24
Sean Roberts <seanorama@gmail.com>
2015-09-02 22:54:52 +0100

    cleanup

--- extras/configs/proxyusers.sh
+++ extras/configs/proxyusers.sh
@@ -4,12 +4,10 @@
 
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 
 source ${__dir}/../ambari_functions.sh
-
 ambari-configs
 
 ## granting root super user rights
