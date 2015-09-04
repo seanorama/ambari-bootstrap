5a321d737e91a75de593dcd5c70bab20054bdbe0
Sean Roberts <seanorama@gmail.com>
2015-09-04 19:28:50 +0100

    as of Ambari 2.1.1 & HDP 2.3.0, this config isn't included. Falcon needs it.

---
+++ extras/falcon/bugfix-oozie-proxyuser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+## magic, don't touch
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+source ${__dir}/../ambari_functions.sh
+
+ambari-configs
+
+${ambari_config_set} oozie.service.ProxyUserService.proxyuser.falcon.groups "users,hadoop-users"
+${ambari_config_set} oozie.service.ProxyUserService.proxyuser.falcon.hosts "*"
