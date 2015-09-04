ce71869035d48004b3a1d3eb54c54ffc9b89c5d2
Sean Roberts <seanorama@gmail.com>
2015-09-04 22:32:39 +0100

    update to accept variables

--- extras/oozie/proxyusers.sh
+++ extras/oozie/proxyusers.sh
@@ -4,9 +4,13 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
-source ${__dir}/../ambari_functions.sh
 
+source ${__dir}/../ambari_functions.sh
 ambari-configs
 
-${ambari_config_set} oozie-site oozie.service.ProxyUserService.proxyuser.falcon.groups "users,hadoop-users"
-${ambari_config_set} oozie-site oozie.service.ProxyUserService.proxyuser.falcon.hosts "*"
+## granting root super user rights
+proxyusers="${proxyusers:-falcon}"
+for user in ${proxyusers}; do
+    ${ambari_config_set} oozie-site oozie.service.ProxyUserService.proxyuser.${user}.groups "users,hadoop-users"
+    ${ambari_config_set} oozie-site oozie.service.ProxyUserService.proxyuser.${user}.hosts "*"
+done
