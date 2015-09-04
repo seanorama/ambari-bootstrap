c96688d2f5f9b6397e970f556e7ff4baf022628f
Sean Roberts <seanorama@gmail.com>
2015-09-04 22:32:09 +0100

    docs

--- extras/atlas/atlas-hive-enable.sh
+++ extras/atlas/atlas-hive-enable.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# Set magic variables for current file & dir
+## This will enable the Hive Bridge to Atlas which allows
+##   Atlas to capture metadata & lineage automatically from Hive
+
+##-----------------------------------------------------------------------
+
+## Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
