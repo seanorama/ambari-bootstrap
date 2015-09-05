7febd5af83b0a55e9d9d58942c5be78c7903b614
Sean Roberts <seanorama@gmail.com>
2015-09-05 19:53:05 +0100

    add jar to ambari

--- extras/ranger/prep-mysql.sh
+++ extras/ranger/prep-mysql.sh
@@ -7,6 +7,8 @@ sudo yum -y -q install mysql-server mysql-connector-java
 sudo chkconfig mysqld on
 sudo service mysqld start
 
+sudo ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar
+
 cat << EOF | mysql -u root
 GRANT ALL PRIVILEGES ON *.* to 'root'@'$(hostname -f)' WITH GRANT OPTION;
 SET PASSWORD FOR 'root'@'$(hostname -f)' = PASSWORD('${db_root_password}');
