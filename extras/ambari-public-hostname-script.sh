049d63561a90e1f96299e8b424f2bd487235131b
Sean Roberts <seanorama@gmail.com>
2015-11-10 11:00:58 -0800

    script using icanhazip.com for simple environments

---
+++ extras/ambari-public-hostname-script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo tee "/etc/ambari-agent/conf/public-hostname-gcloud.sh" > /dev/null <<'EOF'
+curl -Ls -m 5 -4 http://icanhazip.com
+EOF
+
+sudo sed -i.bak "/\[agent\]/ a public_hostname_script=\/etc\/ambari-agent\/conf\/public-hostname-gcloud.sh" /etc/ambari-agent/conf/ambari-agent.ini
+sudo chmod +x /etc/ambari-agent/conf/public-hostname-gcloud.sh
