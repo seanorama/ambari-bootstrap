abcaa4e0819934f96bcd571d6eb8b81b41e5a4b2
Sean Roberts <seanorama@gmail.com>
2015-11-10 11:03:08 -0800

    no message

--- extras/ambari-public-hostname-script.sh
+++ extras/ambari-public-hostname-script.sh
@@ -4,5 +4,5 @@ sudo tee "/etc/ambari-agent/conf/public-hostname-icanhazip.sh" > /dev/null <<'EO
 curl -Ls -m 5 -4 http://icanhazip.com
 EOF
 
-sudo sed -i.bak "/\[agent\]/ a public_hostname_script=\/etc\/ambari-agent\/conf\/public-hostname-gcloud.sh" /etc/ambari-agent/conf/ambari-agent.ini
+sudo sed -i.bak "/\[agent\]/ a public_hostname_script=\/etc\/ambari-agent\/conf\/public-hostname-icanhazip.sh" /etc/ambari-agent/conf/ambari-agent.ini
 sudo chmod +x /etc/ambari-agent/conf/public-hostname-icanhazip.sh
