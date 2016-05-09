bbd62e628e5650a6f46d262f9bccda25616a4200
Sean Roberts <seanorama@gmail.com>
2016-05-09 13:45:47 -0700

    ambari 2.2.2.0 and support changing base of ambari repo url

--- ambari-bootstrap.sh
+++ ambari-bootstrap.sh
@@ -23,9 +23,10 @@ iptables_disable="${iptables_disable:-true}"
 java_provider="${java_provider:-open}" # accepts: open, oracle
 java_version="${java_version:-8}"
 ambari_server="${ambari_server:-localhost}"
-ambari_version="${ambari_version:-2.2.1.1}"
+ambari_version="${ambari_version:-2.2.2.0}"
 ambari_version_major="${ambari_version_major:-$(echo ${ambari_version} | cut -c 1).x}"
 ambari_server_custom_script="${ambari_server_custom_script:-/bin/true}"
+ambari_repo_baseurl=http://public-repo-1.hortonworks.com/ambari
 ##ambari_repo= ## if using a local repo. Otherwise the repo path is determined automatically in a line below.
 curl="curl -ksSL"
 
@@ -67,7 +68,7 @@ if [ -z "${lsb_dist}" ] && [ -r /etc/redhat-release ]; then
 fi
 lsb_dist="$(echo "${lsb_dist}" | tr '[:upper:]' '[:lower:]')"
 
-ambari_repo="${ambari_repo:-http://public-repo-1.hortonworks.com/ambari/${lsb_dist}${lsb_dist_release}/${ambari_version_major}/updates/${ambari_version}/ambari.repo}"
+ambari_repo="${ambari_repo:-${ambari_repo_baseurl}/${lsb_dist}${lsb_dist_release}/${ambari_version_major}/updates/${ambari_version}/ambari.repo}"
 
 if command_exists ambari-agent || command_exists ambari-server; then
     printf >&2 'Warning: "ambari-agent" or "ambari-server" command appears to already exist.\n'
