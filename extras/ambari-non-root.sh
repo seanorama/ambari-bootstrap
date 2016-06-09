65a99e8ad3bdabac9bed7d92a12e0658079815a9
Sean Roberts <seanorama@gmail.com>
2016-06-09 08:08:33 +0100

    Feature/amazon linux (#19)
    * detect amazon linux
    
    * hive transaction in deploy
    
    * make java 8 the default
    
    * apparently 2.4 isn't ready. It somehow slipped in but is disabled in the UI
    
    * add non-root user if not exist & put in hadoop group
    
    * bug: update alternatives with appropriate java version

--- extras/ambari-non-root.sh
+++ extras/ambari-non-root.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-ambari_user=${ambari_user:-ambari}
+ambari_user=${ambari_user:-ambari-server}
+
 
 getent passwd ${ambari_user} > /dev/null
 if [ $? -eq 0 ]; then
-    sudo usermod -a -G users ${ambari_user}
+    sudo usermod -a -G hadoop ${ambari_user}
     printf "y\ny\n${ambari_user}\nn\nn\n" | sudo ambari-server setup
 else
+    useradd -d /var/lib/ambari-server -G hadoop -M -r -s /sbin/nologin ambari-server
+    printf "y\ny\n${ambari_user}\nn\nn\n" | sudo ambari-server setup
     echo "No, the user does not exist"
 fi
 
