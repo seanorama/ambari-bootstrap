08650f17d0b9f02b99ef866c21b837fab093dda3
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2016-09-25 22:09:46 -0700

    Update deploy-recommended-cluster.bash

--- deploy/deploy-recommended-cluster.bash
+++ deploy/deploy-recommended-cluster.bash
@@ -143,7 +143,7 @@ done
 if [ "${recommendation_strategy}" != 'false' ]; then
 cat > ${tmp_dir}/configurations.json <<EOF
 { "resources" : [ {
-  "Versions" : { "stack_name" : ${ambari_stack_name}, "stack_version" : ${ambari_stack_version} },
+  "Versions" : { "stack_name" : "${ambari_stack_name}", "stack_version" : "${ambari_stack_version}" },
   "recommendations" : { "blueprint" : { "configurations" : { "cluster-env" : {
     "properties" : { "ambari-bootstrap-filler" :
       "this does nothing but is needed to merge configurations"
