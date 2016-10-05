283915ac4f99f33ec396f172cc0d9580cb938400
Sean Roberts <seanorama@gmail.com>
2016-10-04 18:06:39 -0700

    switch ambari-server username

--- extras/ambari-non-root.sh
+++ extras/ambari-non-root.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ambari_user=${ambari_user:-ambari-server}
+ambari_user=${ambari_user:-ambari}
 
 
 getent passwd ${ambari_user} > /dev/null
@@ -8,7 +8,7 @@ if [ $? -eq 0 ]; then
     sudo usermod -a -G hadoop ${ambari_user}
     printf "y\ny\n${ambari_user}\nn\nn\n" | sudo ambari-server setup
 else
-    useradd -d /var/lib/ambari-server -G hadoop -M -r -s /sbin/nologin ambari-server
+    useradd -d /var/lib/ambari-server -G hadoop -M -r -s /sbin/nologin ${ambari_user}
     printf "y\ny\n${ambari_user}\nn\nn\n" | sudo ambari-server setup
     echo "No, the user does not exist"
 fi
