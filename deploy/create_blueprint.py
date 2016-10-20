72c6d75d176c1ccdbca9959966026821131e801e
Sean Roberts <seanorama@gmail.com>
2016-10-20 19:03:01 +0100

    remove HIVE_INTERACTIVE

--- deploy/create_blueprint.py
+++ deploy/create_blueprint.py
@@ -32,6 +32,8 @@ def _fix_components(blueprint):
                 components.remove(entry)
         if {'name': 'HIVE_METASTORE'} in components:
             components.append({'name': 'MYSQL_SERVER'})
+        if {'name': 'HIVE_INTERACTIVE'} in components:
+            components.remove({'name': 'HIVE_INTERACTIVE'})
         if {'name': 'HBASE_MASTER'} in components:
             if {'name': 'PHOENIX_QUERY_SERVER'} not in components:
                 components.append({'name': 'PHOENIX_QUERY_SERVER'})
